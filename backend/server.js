644ff996ef8896948449c464ba4511c4cfb97b1f
Ayabonga <mtsoloayabonga@gmail.com>
2026-02-12 10:53:23 +0200

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ const statsRoutes = require("./routes/statsRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://aquamarine-valkyrie-bc9181.netlify.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 mongoose
